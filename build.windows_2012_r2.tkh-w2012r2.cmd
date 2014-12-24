a84f281e4679a3ad922764ca0d51cf80d93789ec
st01tkh <st01tkh@gmail.com>
2014-12-24 21:04:33 +0300

    Fixed some mistakes

--- build.windows_2012_r2.tkh-w2012r2.cmd
+++ build.windows_2012_r2.tkh-w2012r2.cmd
@@ -1,5 +1,13 @@
+@echo off
+SET git_cmd="C:\Program Files (x86)\Git\cmd\git.exe"
+SET pc_dir="D:\Users\Public\VM\packer\w2012r2\packer-windows\packer_cache"
+echo Git command: %git_cmd"
+echo Packer cache dir: %pc_dir"
+%git_cmd% clone https://github.com/joefitzgerald/packer-windows
 cd packer-windows
-mklink tkh-w2012r2.json ..\tkh-w2012r2.json
-packer build tkh-w2012r2.json
+move packer_cache packer_cache.bak
+mklink /j packer_cache %pc_dir%
+mklink windows_2012_r2.tkh-w2012r2.json ..\tkh-w2012r2.json
+packer build windows_2012_r2.tkh-w2012r2.json
 del tkh-w2012r2.json
 cd ..
